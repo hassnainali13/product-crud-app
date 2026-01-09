d91876b319aa91eb31a3c78b4f1e70d3835bd482
HassnainAli <ahassnainsaleem964@gmail.com>
2026-01-09 22:40:34 +0500

    Backend ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ mongoose
   .catch((err) => console.error("MongoDB error:", err));
 
 // Port
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
